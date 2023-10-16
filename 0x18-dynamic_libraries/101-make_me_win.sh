74016805dab3ecccfd4c78cf8df78e053068180d
aymane <skays.dev@gmail.com>
2023-10-16 07:43:47 -0700

    go

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/skays-dev/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
-
+wget -O /tmp/win.so https://github.com/skays-dev/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
